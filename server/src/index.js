b8768cf1b15f9e75d6a5b8385ec49960b6f277a0
Marcelo Reyna <lepozepo@gmail.com>
2020-03-31 13:25:38 -0600

    Update readme's and expose aws-lambda-graphql exports

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,9 @@
 export { default as ApolloServer } from './ApolloServer';
 export { default as PubSub } from './PubSub';
+export {
+  withFilter,
+  DynamoDBEventProcessor,
+  DynamoDBConnectionManager,
+  DynamoDBEventStore,
+  DynamoDBSubscriptionManager,
+} from 'aws-lambda-graphql';
