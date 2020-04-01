0c5dbdaf450ced58c4835c1b7d7f5b8152ad0e45
Marcelo Reyna <lepozepo@gmail.com>
2020-03-31 21:38:01 -0600

    Adapt PubSub to work with both apollo-servers

--- server/src/PubSub.js
+++ server/src/PubSub.js
@@ -2,13 +2,36 @@ import {
   DynamoDBEventStore,
   PubSub as ALGPubSub,
 } from 'aws-lambda-graphql';
+import {
+  PubSub as InMemoryPubSub,
+} from 'apollo-server';
 
-export default class PubSub extends ALGPubSub {
-  constructor({ eventStore } = {}) {
-    super({
-      eventStore: eventStore || new DynamoDBEventStore({
-        eventsTable: process.env.UMBLE_EVENTS_TABLE,
-      }),
-    });
+export default class PubSub {
+  constructor(props = {}) {
+    const {
+      eventStore,
+      dev = false,
+    } = props;
+
+    this.props = props;
+
+    if (dev) {
+      this.engine = new InMemoryPubSub();
+    } else {
+      this.engine = new ALGPubSub({
+        eventStore: eventStore || new DynamoDBEventStore({
+          eventsTable: process.env.UMBLE_EVENTS_TABLE,
+        }),
+      });
+    }
   }
+
+  subscribe = (events) => {
+    if (this.props.dev) {
+      return () => this.engine.asyncIterator(events);
+    }
+    return this.engine.subscribe(events);
+  }
+
+  publish = (event, payload) => this.engine.publish(event, payload);
 }
