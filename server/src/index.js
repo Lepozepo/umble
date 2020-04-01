2b62499aa4e6f355d32d8637fabb1370ec0d1997
Marcelo Reyna <lepozepo@gmail.com>
2020-03-31 21:42:51 -0600

    Use apollo-server withFilter instead
    They're the same but one is one commit or two behind

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,7 @@
 export { default as ApolloServer } from './ApolloServer';
 export { default as PubSub } from './PubSub';
-export { gql } from 'apollo-server';
+export { gql, withFilter } from 'apollo-server';
 export {
-  withFilter,
   DynamoDBEventProcessor,
   DynamoDBConnectionManager,
   DynamoDBEventStore,
