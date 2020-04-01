eece8459b31256ba3fe96e6971a7d54dea89bb17
Marcelo Reyna <lepozepo@gmail.com>
2020-04-01 12:42:16 -0600

    Use babel-node to run

--- server/src/cli.js
+++ server/src/cli.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import cp from 'child_process';
 
 function getArg(args, { arg, defaultValue = null } = {}) {
   if (!arg) return null;
@@ -12,5 +13,5 @@ export default async function run(args) {
   const fn = getArg(args, { arg: '-f', defaultValue: 'dev' });
 
   // eslint-disable-next-line
-  require(path.resolve(process.cwd(), directory))[fn]();
+  cp.execSync(`npx babel-node -e 'require("${path.resolve(process.cwd(), directory)}").${fn}()'`, { stdio: 'inherit' });
 }
