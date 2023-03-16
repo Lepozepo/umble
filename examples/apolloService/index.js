bfc768e5f7dc1ed5ef82b46e66696e1971787633
Marcelo Reyna <Lepozepo@gmail.com>
2023-03-16 08:52:14 -0700

    Remove vpc default

--- examples/apolloService/index.js
+++ examples/apolloService/index.js
@@ -1,9 +1,21 @@
 const pulumi = require('@pulumi/pulumi');
 const awsx = require('@pulumi/awsx');
-const { Service } = require('../../pkg/dist-node');
+const { Service } = require('umble');
+
+const repo = new awsx.ecr.Repository('repo', {
+  forceDelete: true,
+});
+
+const image = new awsx.ecr.Image('image', {
+  repositoryUrl: repo.url,
+  path: './app',
+});
 
 const api = new Service('umble', {
-  image: awsx.ecs.Image.fromPath('umble-test', './app'),
+  image,
+  healthCheckUrl: '/.well-known/apollo/server-health',
 });
 
 exports.url = pulumi.interpolate`http://${api.httpListener.endpoint.hostname}`;
+
+// pulumi stack init -s dev
